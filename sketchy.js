c571a9e10387b3bbe5f2b71234971067004c7ede
Mikey <112041318+dapatcho@users.noreply.github.com>
2024-08-09 12:39:55 -0400

    Back at it progress, grid generates and leaves a trail.

--- sketchy.js
+++ sketchy.js
@@ -1,11 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  gridMaker(16);
+  permaHover();
+});
+
 // create a for loop that creates 16 divs
 
 const gridContainer = document.getElementById("grid-container");
-
 const gridMaker = (num) => {
   for (let i = 0; i < num * num; i++) {
     const gridPixel = document.createElement("div");
     gridPixel.classList.add("grid-pixel");
     gridContainer.appendChild(gridPixel);
   }
+};
+
+const permaHover = () => {
+  const gridPixel = document.querySelectorAll(".grid-pixel");
+  gridPixel.forEach((pixel) => {
+    pixel.addEventListener("mouseover", () => {
+      pixel.classList.add("hovered");
+    });
+  });
 };
