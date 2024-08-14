046aae4e36c828216412454a92a8b059f6e8c52a
Mikey <112041318+dapatcho@users.noreply.github.com>
2024-08-14 15:53:50 -0400

    Sketches and scales to given prompt. "Extra credit" to be done later.

--- sketchy.js
+++ sketchy.js
@@ -3,13 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
   permaHover();
 });
 
-// create a for loop that creates 16 divs
+// create a for loop that creates i*i divs
+
+const containerSize = 800;
 
 const gridContainer = document.getElementById("grid-container");
 const gridMaker = (num) => {
+  gridContainer.innerHTML = "";
+
+  const gridPixelHeight = containerSize / num;
+
   for (let i = 0; i < num * num; i++) {
     const gridPixel = document.createElement("div");
     gridPixel.classList.add("grid-pixel");
+    gridPixel.style.width = `${gridPixelHeight}px`;
+    gridPixel.style.height = `${gridPixelHeight}px`;
     gridContainer.appendChild(gridPixel);
   }
 };
@@ -21,4 +29,20 @@ const permaHover = () => {
       pixel.classList.add("hovered");
     });
   });
+};
+
+const promptNewGrid = () => {
+  let size = prompt("Define how many pixels high the grid will be");
+  size = parseInt(size);
+
+  if (isNaN(size) || size <= 0 || size >= 100) {
+    alert("Please provide a valid positive number less than 100");
+  } else {
+    newGrid(size);
+  }
+};
+
+const newGrid = (num) => {
+  gridMaker(num);
+  permaHover();
 };
